00590799723ae9fff4bfe5d9a70d6f7eb585d2ec
Jacob <111925447+lIJacobIl0002@users.noreply.github.com>
2022-10-05 20:09:19 -0400

    Add files via upload

---
+++ operation_overloading.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+// operator overloading
+std::ostream& operator<<(std::ostream& cout, std::fstream& file_object) {
+    std::string nigger;
+    int i = 0;
+    while (true) {
+        std::getline(file_object, nigger);
+        // file_object >> character;
+        
+
+        cout << nigger << '\n';
+        if (file_object.eof())
+            break;
+    }
+    cout << '\n';
+
+    return cout;
+}
+
+std::istream& operator>>(std::istream& cin, std::fstream& file_object) {
+    std::string file_contents;
+    std::cout << "file contents: ";
+    std::getline(cin, file_contents);
+    
+    // once we have the file contents the user wants
+    file_object << file_contents;
+
+};
+
+using namespace std;
+int main() {
+    fstream file;
+    file.open("filename.txt");
+
+    if (!file) {
+        cout << "file does not exist";
+        return 1;
+    }
+
+    // cin >> file;
+
+    cout << file;
+
+    // file << "sub boys";
+
+    file.close();
+
+    cin.get();
+
+    return 0;
+}
